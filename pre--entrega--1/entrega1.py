b756e5a7bfb3a02b6b5fd724c6c3911356709c47
Shirley Castro <castroshirley24@gmail.com>
2023-09-25 23:34:47 -0300

    Primera pre-entrega

---
+++ pre--entrega--1/entrega1.py
@@ -0,0 +1,74 @@
+import json
+
+# Function to load users from a JSON file
+def load_users():
+    try:
+        with open("users.json", "r") as file:
+            return json.load(file)
+    except FileNotFoundError:
+        return {}
+
+data = {}
+
+# Function to save users to a JSON file
+def save_users(users):
+    data['users'] = []
+    for username, password in users.items():
+        data['users'].append({
+            'username': username,
+            'password': password
+        })
+
+    with open("users.json", "w") as file:
+        json.dump(data, file, indent=4)
+
+# Function to register a user
+def register_user(users):
+    username = input("Enter username: ")
+    password = input("Enter password: ")
+    users[username] = password
+    save_users(users)
+    print("Registration successful.")
+
+# Function to display registered users
+def show_users(users):
+    if not users:
+        print("No registered users.")
+    else:
+        print("Registered users:")
+        for username, password in users.items():
+            print(f"Username: {username}, Password: {password}")
+            print("---------------")
+
+# Function for user login
+def login(users):
+    username = input("Enter your username: ")
+    password = input("Enter your password: ")
+    if username in users and users[username] == password:
+        print("Login successful.")
+    else:
+        print("Incorrect username or password.")
+
+# Load existing users from the JSON file
+users = load_users()
+
+# Main menu
+while True:
+    print("1. Register User")
+    print("2. Show Registered Users")
+    print("3. Login")
+    print("4. Exit")
+    option = input("Select an option: ")
+
+    if option == "1":
+        register_user(users)
+    elif option == "2":
+        show_users(users)
+    elif option == "3":
+        login(users)
+    elif option == "4":
+        save_users(users)
+        print("Goodbye!")
+        break
+    else:
+        print("Invalid option. Please try again.")
