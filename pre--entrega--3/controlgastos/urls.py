00468aae7d098d3d8cad9f065612e37d0e813d11
Shirley Castro <castroshirley24@gmail.com>
2023-11-02 20:41:19 -0300

    Agregando template de bootstrap, ordenando la navegabilidady los botones del navbar

--- pre--entrega--3/controlgastos/urls.py
+++ pre--entrega--3/controlgastos/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from controlgastos.views import inicio, categoria_transaccion, transaccion, presupuesto_transaccion, mes
 
 urlpatterns = [
-    path('', inicio),
-    path('categoria-transaccion/', categoria_transaccion),
-    path('transaccion/', transaccion),
-    path('presupuesto-transaccion/', presupuesto_transaccion),
-    path('mes/', mes)
+    path('', inicio, name='inicio'),
+    path('categoria-transaccion/', categoria_transaccion, name='categoria'),
+    path('transaccion/', transaccion, name='transaccion'),
+    path('presupuesto-transaccion/', presupuesto_transaccion, name='presupuesto'),
+    path('mes/', mes, name='mes')
 ]
